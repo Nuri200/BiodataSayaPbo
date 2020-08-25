c0d0a317231d5946ce55af65388cd8f81d554a11
Nuri200 <70131629+Nuri200@users.noreply.github.com>
2020-08-25 09:55:36 +0700

    Add files via upload

---
+++ Biodata_nuri.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package biodata_nuri;
+
+/**
+ *
+ * @author ENDANG
+ */
+public class Biodata_nuri {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String nama="Nuri Astutik";
+        String Jk="Perempuan";
+        String Alamat="Jl.Raya Makam Purwosari, Kacangbayi";
+        String Jurusan="XI RPL 1";
+        
+        System.out.println("biodata saya");//mengeprint atau mencetak
+        System.out.println("--------");
+        System.out.println("Nama \t\t\t:"+nama);
+        System.out.println("Jk \t\t\t:"+Jk);
+        System.out.println("Alamat \t\t\t:"+Alamat);
+        System.out.println("Jurusan \t\t\t:"+Jurusan);
+    }
+}
